da26a26952d6e30ddc9a73d8ba1810779ada7322
raguram5105 <2315015@nec.edu.in>
2026-06-20 21:31:16 +0530

    env setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express"
+import dotenv from "dotenv";
+
 const app=express();
-const port=3000;
-app.listen(port,()=>{
-    console.log(`The server is running in port : ${port}`)
+dotenv.config();
+const PORT = process.env.PORT
+app.listen(PORT,()=>{
+    console.log(`The server is running in port : ${PORT}`)
 })
