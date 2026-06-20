905ea7479dbcb6d0ac1da79461f023e97cd9af6e
raguram5105 <2315015@nec.edu.in>
2026-06-20 22:46:21 +0530

    user creation completed

--- backend/Controller/userController.js
+++ backend/Controller/userController.js
@@ -1,2 +1,15 @@
-import user from "../Models/User.js"
+import {User} from "../models/User.js";
 
+export const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const user = await User.create({
+            name,
+            email,
+            password
+        });
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import userRoutes from "./Routes/userRoutes.js";
 
 dotenv.config();
-
 connectDB();
-
 const app = express();
-
-const PORT = process.env.PORT || 5000;
-
+app.use(express.json());
+app.use("/api/auth", userRoutes);
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`The server is running in port : ${PORT}`);
 });
