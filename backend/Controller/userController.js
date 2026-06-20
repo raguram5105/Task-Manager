ef86f6bb161b9fe43a8fe674aa6fd61a99331ad6
raguram5105 <2315015@nec.edu.in>
2026-06-20 22:21:21 +0530

    db connected

---
+++ backend/Controller/userController.js
@@ -0,0 +1,2 @@
+import user from "../Models/User.js"
+
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
-import express from "express"
+import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
-const app=express();
 dotenv.config();
-const PORT = process.env.PORT
-app.listen(PORT,()=>{
-    console.log(`The server is running in port : ${PORT}`)
-})
+
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`The server is running in port : ${PORT}`);
+});
