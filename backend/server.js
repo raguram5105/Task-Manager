963ab9eb4e5e25095b19e7c819396ec91fb40da2
raguram5105 <2315015@nec.edu.in>
2026-06-22 12:43:13 +0530

    frontend setup

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRoutes from "./Routes/userRoutes.js";
+import cors from "cors";
+
 
 dotenv.config();
 connectDB();
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", userRoutes);
 const PORT = process.env.PORT;
